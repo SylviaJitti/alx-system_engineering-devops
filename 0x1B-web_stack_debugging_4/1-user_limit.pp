6ca76889efb12eecbfeb287fe047a28e459cffec
SylviaJitti <sylviajitti@gmail.com>
2023-05-17 23:35:49 -0700

    Fixing the nginx-conf and limitations

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,18 +1,13 @@
-# This file Changes the OS configuration so that it is possible to login with 
-# the holberton user and open a file without any error message.
-file { '/etc/security/limits.conf':
-  ensure  => present,
-  content => "holberton hard nofile 88888\nholberton soft nofile 88888\n",
+# Enable the user holberton to login and open files without error.
+
+# Increase hard file limit for Holberton user.
+exec { 'increase-hard-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
 
-exec { 'reload-limits-configuration':
-  command => 'pam-auth-update --force',
-  path    => '/usr/bin:/usr/sbin:/bin',
-  refreshonly => true,
-}
-
-service { 'systemd-logind':
-  ensure => running,
-  enable => true,
-  require => [File['/etc/security/limits.conf'], Exec['reload-limits-configuration']],
+# Increase soft file limit for Holberton user.
+exec { 'increase-soft-file-limit-for-holberton-user':
+  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf',
+  path    => '/usr/local/bin/:/bin/'
 }
